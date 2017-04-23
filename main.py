bd62689cf33c4f48b2eb5584fb1e13892b31f636
b0204888100 <b0204888100@gmail.com>
2017-04-24 00:18:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ app.config['DEBUG'] = True
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
+INSTANCE_NAME = 'instance-1'
+INSTANCE_ZONE = 'asia-east1-a'
+PROJECT = 'esoteric-cider-165206'
 
 
 @app.route('/')
@@ -11,7 +14,13 @@ def hello():
     """Return a friendly HTTP greeting."""
     return 'Hello World!'
 
-
+@app.route('/vm/start')
+def start_vm():
+    credentials = AppAssertionCredentials(scope='https://www.googleapis.com/auth/compute')
+    http = credentials.authorize(httplib2.Http(memcache))
+    compute = discovery.build('compute', 'v1', http=http)
+    result = compute.instances().start(instance=INSTANCE_NAME, zone=INSTANCE_ZONE, project=PROJECT).execute()
+  
 @app.errorhandler(404)
 def page_not_found(e):
     """Return a custom 404 error."""
