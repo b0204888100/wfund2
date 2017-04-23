5f956555c6e1ff765d4e6c9106ae1ea8d37a85f9
b0204888100 <b0204888100@gmail.com>
2017-04-24 00:26:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -16,9 +16,6 @@ def hello():
 
 @app.route('/vm/start')
 def start_vm():
-    credentials = AppAssertionCredentials(scope='https://www.googleapis.com/auth/compute')
-    http = credentials.authorize(httplib2.Http(memcache))
-    compute = discovery.build('compute', 'v1', http=http)
     result = compute.instances().start(instance=INSTANCE_NAME, zone=INSTANCE_ZONE, project=PROJECT).execute()
   
 @app.errorhandler(404)
